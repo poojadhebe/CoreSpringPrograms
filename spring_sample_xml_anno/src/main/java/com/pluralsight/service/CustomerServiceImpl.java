248103c6700a0bcc25df9dffcb4f287f3114cd6a
Pooja Dhebe <poojadhebe@gmail.com>
2019-07-10 23:02:22 +0530

    Spring programs

--- spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,13 +2,18 @@ package com.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-	CustomerRepository customerRepository=new HibernateCustomerRepositoryImpl();
+	//Member variable autowiring
+	//@Autowired
+	CustomerRepository customerRepository;
 	
 	/* (non-Javadoc)
 	 * @see com.pluralsight.service.CustomerService#findAll()
@@ -16,5 +21,30 @@ public class CustomerServiceImpl implements CustomerService {
 	public List<Customer> findAll(){
 		return customerRepository.findAll();
 	}
+
+	/**
+	 * @param customerRepository the customerRepository to set
+	 */
+	//Setter injection autowiring
+	//@Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+		System.out.println("Using setter injection");
+	}
+
+	
+	public CustomerServiceImpl() {
+		super();
+	}
+	@Autowired
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		super();
+		System.out.println("Using constructor injection");
+		this.customerRepository = customerRepository;
+	}
+	
+	
+	
+	
 	
 }
